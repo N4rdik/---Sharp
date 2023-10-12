d98246ca6bb4f66f6a13e891d5669225c40bce44
N4rdik <nardik@inbox.ru>
2023-10-12 14:20:12 +0300

    step by step solving tasks

---
+++ task001_проверка_квадрата/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 1. Проверить два введенных числа - квадрат ли это?
+*/
+using System;
+
+System.Console.WriteLine("Ведите два числа: ");
+
+int userNumber1 = System.Convert.ToInt32(System.Console.ReadLine());
+int userNumber2 = System.Convert.ToInt32(Console.ReadLine());
+
+if (userNumber1 / userNumber2 == user_number_2)
+{
+    System.Console.WriteLine($"Введеное первое число являеться квдратом второго");
+}
+else
+{
+    System.Console.WriteLine($"Введеное первое число не являеться квадратом второго");
+}
