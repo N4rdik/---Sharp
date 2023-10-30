9fd08d87db316211523528d4db0f3bd461c9b83f
N4rdik <nardik@inbox.ru>
2023-10-30 12:16:27 +0300

    Added some tasks

--- task044_fibonachi/Program.cs
+++ task044_fibonachi/Program.cs
@@ -1,39 +1,37 @@
-﻿/*
-"Задача 44: Не используя рекурсию, выведите первые N чисел
-Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
-Если N = 5 -> 0 1 1 2 3
-Если N = 3 -> 0 1 1
-Если N = 7 -> 0 1 1 2 3 5 8"
-*/
-using System;
+﻿using System;
 Console.Clear();
 
-
-int TakeEnteredNumber(string message)
+int Prompt(string message)
 {
-    System.Console.WriteLine(message);
+    System.Console.Write(message);
     int result = Convert.ToInt32(Console.ReadLine());
     return result;
 }
 
-string FebonchiNumber(int a)
+int[] Fibonacci(int number)
 {
-    string result = "0 1";
-    int summ = 0;
-    if(a == 0) result = result + "0";
-    if(a == 1) result = result + "0 1";
-    else
-    {
-        for (int i = 2; i <= a; i++)
-        {
-            result = result + (i-1) + (i-2);    
-        }
-    }
-    return result;
+    int size = number;
+    if (number < 2) size = 2;
+    int[] array = new int[size];
+    array[0] = 0;
+    array[1] = 1;
 
+    for (int i = 2; i < size; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
 }
 
-int userNumber = TakeEnteredNumber("Enter number: ");
-
-
-System.Console.WriteLine(FebonchiNumber(userNumber));
+void PrintArray(int[] arrayOne)
+{
+    System.Console.Write("Number N = " + arrayOne.Length + ": ");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        System.Console.Write(" " + arrayOne[i]);
+    }
+    System.Console.WriteLine();
+}
+int number = Prompt("Enter number: ");
+PrintArray(Fibonacci(number));
