e373af87058df0c39abe2fd7707a26ef15079151
N4rdik <nardik@inbox.ru>
2023-11-23 12:13:25 +0300

    all tasks solved - course com;etedd

--- task060/Program.cs
+++ task060/Program.cs
@@ -4,3 +4,46 @@
 двузначных чисел. Напишите программу, которая будет построчно выводить
 массив, добавляя индексы каждого элемента.*/
 
+/*
+Сформируйте трёхмерный массив из неповторяющихся
+двузначных чисел. Напишите программу, которая будет построчно выводить
+массив, добавляя индексы каждого элемента.
+*/
+using System;
+Console.Clear();
+
+
+
+int[,,] array3D = new int[3, 3, 3];
+
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"{array3D[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+void FillArray(int[,,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+FillArray(array3D);
+PrintIndex(array3D);
--- task064/Program.cs
+++ task064/Program.cs
@@ -4,4 +4,26 @@ N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
 using System;
+Console.Clear();
+
+int TakeEnteredNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void NaturalNumberPrint (int number)
+{
+    if (number == 0) return;
+    else
+        System.Console.Write(number + " ");
+        NaturalNumberPrint(number - 1);
+}
+
+
+
+int userNumber = TakeEnteredNumber("Введите число");
+
+NaturalNumberPrint(userNumber);
 
